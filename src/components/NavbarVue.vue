e0b69c2dd5848d46d0402023d57e17c3462d3355
Reza Zeraat <rzeraat.tur@gmail.com>
2022-09-22 02:18:51 +0300

    container style for navbar

--- src/components/NavbarVue.vue
+++ src/components/NavbarVue.vue
@@ -1,3 +1,22 @@
+<script setup>
+import IconContrastVue from "./icons/IconContrast.vue";
+import SpaceVue from "./main/SpaceVue.vue";
+</script>
 <template>
-  <section>Navbar</section>
+  <section class="container">
+    <SpaceVue scope="block" size="50px" />
+    <IconContrastVue />
+    <SpaceVue scope="block" size="50px" />
+  </section>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  background-color: #fff;
+}
+</style>
