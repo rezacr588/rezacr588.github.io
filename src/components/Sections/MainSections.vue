eabeb5fa1995e35f25b092c5a50e8d5b83f6e1ff
Reza Zeraat <rzeraat.tur@gmail.com>
2022-09-26 01:24:17 +0300

    add Prices section

--- src/components/Sections/MainSections.vue
+++ src/components/Sections/MainSections.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Services from "./Services.vue";
 import HeaderVue from "../HeaderVue.vue";
+import Prices from "./Prices.vue";
 </script>
 <template>
   <main>
     <HeaderVue />
     <Services />
+    <Prices />
   </main>
 </template>
---
+++ src/components/Sections/Prices.vue
@@ -0,0 +1,12 @@
+<script setup>
+import SectionBodyLayout from "../main/SectionBodyLayout.vue";
+</script>
+
+<template>
+  <SectionBodyLayout
+    id="price"
+    title="Pay rate"
+    description="My pay rate according to titles"
+  >
+  </SectionBodyLayout>
+</template>
