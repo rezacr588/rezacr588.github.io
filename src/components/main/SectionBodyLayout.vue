3ad7ac553755b8613dea67339ca69ca843dc3f9b
Reza Zeraat <rzeraat.tur@gmail.com>
2022-09-29 20:11:27 +0300

    set max width for description

--- src/components/main/SectionBodyLayout.vue
+++ src/components/main/SectionBodyLayout.vue
@@ -8,9 +8,11 @@ const props = defineProps(["id", "title", "description"]);
   <section :id="props.id">
     <Typography component="h2" color="#2B2B2B"> {{ props.title }} </Typography>
     <SpaceVue />
-    <Typography component="p" color="#767676">
-      {{ props.description }}
-    </Typography>
+    <div class="description-container">
+      <Typography component="p" color="#767676">
+        {{ props.description }}
+      </Typography>
+    </div>
     <SpaceVue size="1rem" />
     <slot></slot>
   </section>
@@ -20,5 +22,9 @@ const props = defineProps(["id", "title", "description"]);
 section {
   margin: 70px auto;
   text-align: center;
+}
+.description-container {
+  max-width: 600px;
+  margin: 0 auto;
 }
 </style>
