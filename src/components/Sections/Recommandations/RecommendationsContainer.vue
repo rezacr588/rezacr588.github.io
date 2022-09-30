4b4cf75e13b26ee9a6bbe6f6af8793ad313de3b0
Reza Zeraat <rzeraat.tur@gmail.com>
2022-09-30 16:01:11 +0300

    add slider to recommendations

--- src/components/Sections/Recommandations/RecommendationsContainer.vue
+++ src/components/Sections/Recommandations/RecommendationsContainer.vue
@@ -7,13 +7,42 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      items: this.recommendations,
+      current: 1,
+    };
+  },
   components: { RecommendationItem },
+  computed: {
+    dotsCount() {
+      return Math.ceil(this.recommendations.length / 3);
+    },
+    showedItems() {
+      return this.items.slice((this.current - 1) * 3, this.current * 3);
+    },
+  },
+  methods: {
+    next() {
+      if (this.current < this.dotsCount - 1) {
+        this.current++;
+      }
+    },
+    prev() {
+      if (this.current > 0) {
+        this.current--;
+      }
+    },
+    goTo(index) {
+      this.current = index;
+    },
+  },
 };
 </script>
 <template>
   <div class="container">
     <RecommendationItem
-      v-for="recommendation in recommendations"
+      v-for="recommendation in showedItems"
       :key="recommendation.opinion.title"
       :title="recommendation.opinion.title"
       :description="recommendation.opinion.description"
@@ -23,6 +52,9 @@ export default {
       :picture="recommendation.picture"
     />
   </div>
+  <div class="dot-container">
+    <div :key="i" v-for="i in dotsCount" class="dot" v-on:click="goTo(i)" />
+  </div>
 </template>
 
 <style scoped>
@@ -30,5 +62,16 @@ export default {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   gap: 20px;
+}
+.dot-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+.dot {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: #ffb400;
 }
 </style>
--- src/data/index.js
+++ src/data/index.js
@@ -2,7 +2,7 @@ export const recommendations = [
   {
     id: 1,
     opinion: {
-      title: "Great Quality!",
+      title: "Great Quality! 1",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna viverra morbi. Morbi donec amet....",
       score: 5,
@@ -14,7 +14,7 @@ export const recommendations = [
   {
     id: 2,
     opinion: {
-      title: "Great Quality!",
+      title: "Great Quality! 2",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna viverra morbi. Morbi donec amet....",
       score: 5,
@@ -26,7 +26,31 @@ export const recommendations = [
   {
     id: 3,
     opinion: {
-      title: "Great Quality!",
+      title: "Great Quality! 3",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna viverra morbi. Morbi donec amet....",
+      score: 5,
+    },
+    name: "James Gouse",
+    jobTitle: "Graphic Designer",
+    picture: "https://randomuser.me/api/portraits/men/75.jpg",
+  },
+  {
+    id: 4,
+    opinion: {
+      title: "Great Quality! 4",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna viverra morbi. Morbi donec amet....",
+      score: 5,
+    },
+    name: "James Gouse",
+    jobTitle: "Graphic Designer",
+    picture: "https://randomuser.me/api/portraits/men/75.jpg",
+  },
+  {
+    id: 5,
+    opinion: {
+      title: "Great Quality! 5",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna viverra morbi. Morbi donec amet....",
       score: 5,
